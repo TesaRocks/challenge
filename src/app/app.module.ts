de2462441305c98d0fa33ba7854855185a3830c0
Alexis Dietl <alexisdietl@gmail.com>
2021-07-30 15:23:31 -0300

    CH7 project effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { environment } from '../environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,6 +18,7 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     ProjectModule,
     StoreModule.forRoot({}),
+    EffectsModule.forRoot(),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production,
--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -4,12 +4,19 @@ import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectNewComponent } from './project-new/project-new.component';
 import { ProjectEditComponent } from './project-edit/project-edit.component';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromProject from './ngrx/project.reducer';
+import { ProjectEffects } from './ngrx/project.effects';
 @NgModule({
   declarations: [
     ProjectListComponent,
     ProjectNewComponent,
     ProjectEditComponent,
   ],
-  imports: [CommonModule, StoreModule],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromProject.projectsFeatureKey, fromProject.reducer),
+    EffectsModule.forFeature([ProjectEffects]),
+  ],
 })
 export class ProjectModule {}
